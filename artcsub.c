6c72147cb3b59bc6672cd5b839600b1aa5149353
Efraim Dahl <efraimdahl@uchicago.edu>
2020-04-14 12:02:17 -0500

    reformatted the code

--- artcsub.c
+++ artcsub.c
@@ -1,71 +1,81 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <math.h>                                   
+                               #include <stdio.h>
+                                                          
+                            // \\ 
+                           // S \\
+                          //  H  \\ 
+                         //   A   \\
+                        //    R    \\      
+                       //     P     \\  
+                      //             \\ 
+                     //       !       \\
+                    //        !        \\
+                   //         !         \\
+                  //I N S T R U C T I O N\\
+                 //this code prints arrows\\
+                //P L E A S E BE C A T IOUS\\
+               //C o m p i l e w i t h g c c\\
+              //T h e n run a . o u t fromthe\\
+             //T T E E R R M M I I N N A A L L\\
+            //DONTHURTYOURSELFPLSIWOULDFEELBAD!\\ 
+          
+                            #include <string.h>
+          #include <stdlib.h>
+                                #include <math.h>       
+           void arrows(){
+            for(int arrow = 0; arrow<10000;arrow++){
+                    int f = arrow%10;
+                          switch(f){
 
-                                                 // \\ 
-                                                // S \\
-                                               //  H  \\ 
-                                              //   A   \\
-                                             //    R    \\      
-                                            //     P     \\  
-                                           //             \\ 
-                                          //       !       \\
-                                         //        !        \\
-                                        //         !         \\
-                                       //I N S T R U C T I O N\\
-                                      //this code prints arrows\\
-                                     //P L E A S E BE C A T IOUS\\
-                                    //C o m p i l e w i t h g c c\\
-                                   //T h e n run a . o u t fromthe\\
-                                  //T T E E R R M M I I N N A A L L\\
-                                 //DONTHURTYOURSELFPLSIWOULDFEELBAD!\\ 
-
-void arrows(){
-    for(int i = 0; i<10000;i++){
-        int f = i%10;
-        switch(f) {
-
-            case 0:
+                            case 0:
                 printf("O||||||||||||||||O\n");
-                break;
-            case 1:
+                            break;
+                            case 1:
                 printf("OO||||||||||||||OO\n");
-                break;
-            case 2:
+                            break;
+                            case 2:
                 printf("OO||||||||||||||OO\n");
-                break;
-            case 3:
+                            break;
+                            case 3:
                 printf("OOO||||||||||||OOO\n");
-                break;
-            case 4:
+                            break;
+                            case 4:
                 printf("OOOO||||||||||OOOO\n");
-                break;
-            case 5:
+                            break;
+                            case 5:
                 printf("OOOOO||||||||OOOOO\n");
-                break;
-            case 6:
+                            break;
+                            case 6:
                 printf("OOOOOO||||||OOOOOO\n");
-                break;
-            case 7:
+                            break;
+                            case 7:
                 printf("OOOOOOO||||OOOOOOO\n");
-                break;
-            case 8:
+                            break;
+                            case 8:
                 printf("OOOOOOOO||OOOOOOOO\n");
-                break;
-            case 9:
+                            break;
+                            case 9:
                 printf("OOOOOOOOOOOOOOOOOO\n");
-                break;
-        }
-    }
-    return;
-}
+                            break;
+                         }}return;}
 
-int main(int argc, char*argv[]){
-    arrows();
-    return 0;
-}
-
-
-
-    
+                int main(int argc, char*argv[]){
+                         arrows();
+                         return 0;}
+              /*
+              \\  YET so Pleasing the pain is  // 
+               \\S O S O F T I S T H E D A R T//
+                \\ T h a t a t once i t b oth//
+                 \\ ! !W O U N D S  M E ! ! //   
+                  \\ A N D   t i c k l e s // 
+                   \\          M          //
+                    \\         Y         //
+                     \\                 //
+                      \\       H       //
+                       \\      E      //
+                        \\     A     //
+                         \\    R    //
+                          \\   T   //
+                           \\     //
+                            \\   //
+                             \\ //
+                              */                          
